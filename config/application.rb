f312e187c8023f410cdb280d53d74ce1d42e36c5
Felix Knoepke <morytox@hotmail.de>
2013-07-01 16:33:06 +0200

    precompiling true

--- config/application.rb
+++ config/application.rb
@@ -59,8 +59,12 @@ module Bookcontrol
     # parameters by using an attr_accessible or attr_protected declaration.
     # config.active_record.whitelist_attributes = true
 
+
     # Enable the asset pipeline
     config.assets.enabled = true
+
+    config.assets.precompile = true
+    config.assets.compile = true
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
