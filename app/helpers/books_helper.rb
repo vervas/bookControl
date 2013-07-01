4ea49001fd42960dd13b2775dd4786ffc4b396ab
Matthias Wiesner <mw@cloudcontrol.de>
2013-07-01 18:57:23 +0200

    fix bug

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,7 +1,7 @@
 module BooksHelper
 
   def self.available(book_id)
-    reservations = Reservation.where(:book_id => book_id)
+    reservations = Reservation.where(:book_id => book_id.to_s)
     return true if reservations.count == 0
     return reservations.all? { |r| r.return_date != nil }
   end
