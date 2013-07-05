24513ac0090dfebe38b27334b455d0e497af2024
Matthias Wiesner <mw@cloudcontrol.de>
2013-07-05 15:20:18 +0200

    Release book if reserved

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -5,4 +5,12 @@ module BooksHelper
     return true if reservations.count == 0
     return reservations.all? { |r| r.return_date != nil }
   end
+
+  def self.isReserved(book_id, user_id)
+    reservations = Reservation.
+      where(:book_id => book_id.to_s).
+      and(:user_id => user_id.to_s).
+      and(:return_date => nil)
+     return reservations.first if reservations.count > 0
+  end
 end
